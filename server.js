4b8e9b02f9d91727736d8297c929043c89121155
SHEY Louis <sheylouis@mbp-de-shey.lan>
2021-07-25 13:09:53 +0200

    resquest-response with Nodejs

--- server.js
+++ server.js
@@ -1,6 +1,43 @@
 const http = require('http')
-const server = http.createServer((req, res) => {
-    console.log('request made')
+const fs = require('fs')
+
+const server = http.createServer((request, response) => {
+    console.log(request.url, request.method)
+
+    //set header content type
+    response.setHeader('Content-Type', 'text/html')
+
+    let path = './views/'
+    switch(request.url) {
+        case '/':
+            path += 'index.html';
+            response.statusCode = 200
+            break;
+        case '/about':
+            path += 'about.html';
+            response.statusCode = 200
+            break;
+        case '/about-me':
+            response.statusCode = 301
+            response.setHeader('Location', '/about')
+            response.end()
+            break;
+        default:
+            path += '404.html';
+            response.statusCode = 404
+            break;
+
+    }
+
+    fs.readFile(path, (err, data) => {
+        if(err) {
+            console.log(err)
+            response.end()
+        }
+        response.write(data)
+        response.end()
+    })
+    
 })
 
 server.listen(3001, 'localhost', () =>{
