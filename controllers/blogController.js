8b80a6d0d85ba2a27446bb814705da51daa074a2
shloch <shloch2007@yahoo.fr>
2021-07-31 17:38:35 +0200

    MVC organisation

---
+++ controllers/blogController.js
@@ -0,0 +1,55 @@
+// blog_index, blod_details, blog_create_get, blog_create_post, blog_delete
+
+const Blog = require('../models/blog')
+
+
+// GET
+const blog_index = (request, response) => {
+    Blog.find().sort({createdAt: -1}) //sort by descending order
+    .then(result => {
+        response.render('blogs/index', {title: 'all blogs', blogs: result})
+    })
+    .catch(error => console.log(error))
+}
+
+const blog_details = (request, response) => {
+    const id = request.params.id
+    console.log(request.params)
+    Blog.findById(id)
+    .then(result => {
+        response.render('blogs/details', {blog: result, title: 'Blog Details'})
+    })
+    .catch(errors => console.log(errors))
+}
+
+
+const blog_create_get = (request, response) => {
+    response.render('blogs/create', {title: 'Create new blog'})
+}
+
+
+// POST
+const blog_create_post = (request, response) => {
+    const blog = new Blog(request.body)
+    blog.save()
+    .then(result => response.redirect('/'))
+    .catch(errors => console.log(errors))
+}
+
+// DELETE
+const blog_delete = (request, response) => {
+    const id = request.params.id
+    Blog.findByIdAndDelete(id)
+    .then(result => {
+        response.json({redirectURL: '/blogs'})
+    })
+   .catch(errors => console.log(errors))
+}
+
+module.exports = {
+    blog_index,
+    blog_details,
+    blog_create_get,
+    blog_create_post,
+    blog_delete
+}
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,50 +1,19 @@
 
 const express = require('express')
 const router = express.Router()
-const Blog = require('../models/blog')
+const blogcontroller = require('../controllers/blogController')
 
 // GET
-router.get('/', (request, response) => {
-    Blog.find().sort({createdAt: -1}) //sort by descending order
-    .then(result => {
-        response.render('index', {title: 'all blogs', blogs: result})
-    })
-    .catch(error => console.log(error))
-})
-
-
-router.get('/create', (request, response) => {
-    response.render('create', {title: 'Create new blog'})
-})
-
-router.get('/:id', (request, response) => {
-    const id = request.params.id
-    console.log(request.params)
-    Blog.findById(id)
-    .then(result => {
-        response.render('details', {blog: result, title: 'Blog Details'})
-    })
-    .catch(errors => console.log(errors))
-})
+router.get('/', blogcontroller.blog_index)
+router.get('/create', blogcontroller.blog_create_get)
+router.get('/:id', blogcontroller.blog_details)
 
 // POST
-router.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-    blog.save()
-    .then(result => response.redirect('/'))
-    .catch(errors => console.log(errors))
- })
+router.post('/', blogcontroller.blog_create_post)
  
-
 // DELETE
-router.delete('/:id', (request, response) => {
-    const id = request.params.id
-    Blog.findByIdAndDelete(id)
-    .then(result => {
-        response.json({redirectURL: '/blogs'})
-    })
-   .catch(errors => console.log(errors))
-})
+router.delete('/:id', blogcontroller.blog_delete)
+
 
 module.exports = router
 
